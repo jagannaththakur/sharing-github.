2cbcd49374fe403f40980578338545b6d27373a2
Jagannath Thakur <jagannaththakur@gmail.com>
2023-10-26 23:38:48 -0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 1. Create a code cell to multiply and add numbers
+# 2. Create a code cell to convert minutes to hours
+# 3. Create a markdown cell with a table of Data Science tools
+# 
+# 
+
+# ###### Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Java
+
+# ###### Some of the commonly used libraries used by Data Scientists include:
+# 1. TensorFlow
+# 2. NumPy
+# 3. SciPy 
+# 4. Pandas
+
+# |Data Science Tools |
+# |RStudio|
+# |Apache Spark|
+# |TensorFlow|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[23]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+min=200
+h=min//60
+m=min%60
+print("Hours=",h)
+print("Minutes=",m)
+
+
+# ## Author
+# 
+# Jagannath Thakur
